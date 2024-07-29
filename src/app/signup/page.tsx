47a9907a2110c4d2feeb901ab46e671947d21d17
panpan <panpan31415@gmail.com>
2024-07-29 11:07:03 +0200

    add login and signUp route

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "PetSoft - Sign Up",
+};
+
+export default function SignUpPage() {
+    return <main>sign up</main>;
+}
